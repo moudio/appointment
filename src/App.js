901cb6227d612d59127dbc867c03ee9ea818758b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 18:21:43 +0000

    install useEffect for getting cars after the App component mounts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Cars from './containers/Cars';
 import { connect } from 'react-redux';
 function App() {
+  useEffect(() => {
+    getCars();
+  });
+
+  function getCars() {
+    axios.get('/api/v1/cars').then((response) => console.log(response.data));
+  }
+
   return (
     <div className="App">
       <Cars />
@@ -10,7 +19,11 @@ function App() {
   );
 }
 
-const mapStateToProps = (state) => {};
+const mapStateToProps = (state) => {
+  return {
+    cars: state.cars,
+  };
+};
 
-const mapDispatchToProps = (props) => {};
+const mapDispatchToProps = (dipsatch) => {};
 export default connect(mapStateToProps, mapDispatchToProps)(App);
