630439f36b08cff6ec84f6fb2213a53de026585d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 17:56:32 +0000

    install the middleware and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+import carReducer from './reducers/car_reducer';
 import './index.css';
 import App from './App';
-
+const store = createStore(carReducer, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
