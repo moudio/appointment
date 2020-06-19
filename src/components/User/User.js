8ad54233bd21dbf5a304028bbd6356df9f1bb68c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-19 23:32:53 +0000

    Add content to the user page

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import './User.css';
+import profile from '../../Images/profile.jpg';
 function User({ user }) {
   return (
-    <div>
-      <h1>Welcome {user.username}</h1>
+    <div className="user-div">
+      <div className="profile">
+        <div className="picture-div">
+          <img src={profile} alt="profile" className="picture-img" />
+        </div>
+        <div className="profile-details">
+          <h1>{user.username}</h1>
+          <p>Car lover</p>
+        </div>
+        <div className="profile-content">
+          <div className="profile-content-navigation">
+            <Router>
+              <ul>
+                <Link to="/books">Books</Link>
+                <Link to="/books">Books</Link>
+              </ul>
+            </Router>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
